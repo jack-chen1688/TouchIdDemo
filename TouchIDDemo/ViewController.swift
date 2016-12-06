2157b15c42953c2a7f264025ebf50698c0739787
Xuehua Chen <xuehuachen@Xuehuas-MacBook-Pro.local>
2016-12-05 21:34:52 -0800

    Initial Commit

---
+++ TouchIDDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIDDemo
+//
+//  Created by Xuehua Chen on 12/5/16.
+//  Copyright © 2016 Xuehua Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
