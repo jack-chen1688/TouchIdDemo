66d5cbc13bcc55a4899c3895f9e53f13cbc89df0
Xuehua Chen <xuehuachen@Xuehuas-MacBook-Pro.local>
2016-12-06 21:33:44 -0800

    Updated a couple of changes.

--- TouchIDDemo/ViewController.swift
+++ TouchIDDemo/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var error: NSError?
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let authenticationContext = LAContext()
+        
+        if authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+        
+            authenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authenticate with Touch ID") { (success, error) in
+            
+                if success {
+                        print ("User has authenticated")
+                } else {
+                    if let error = error {
+                        print (error)
+                    } else {
+                        print ("Did not authenticate")
+                    }
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
