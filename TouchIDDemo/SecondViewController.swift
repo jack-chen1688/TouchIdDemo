24b61215afc6caa5d81d0c88bc3cfc5d2c0f3347
xuehua <xuehua@gmail.com>
2016-12-27 20:51:19 -0800

    Make second view controller to use SecondViewController class and print a message when it is loadedl

---
+++ TouchIDDemo/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  TouchIDDemo
+//
+//  Created by Xuehua Chen on 12/27/16.
+//  Copyright © 2016 Xuehua Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second View Controller loaded.")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
